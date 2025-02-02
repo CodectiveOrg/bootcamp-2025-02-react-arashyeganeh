e79a9fda94acc27cc163f36cc9bac072ef136914
Arash Yeganeh <arashkosariyeganeh@gmail.com>
2025-02-02 18:26:04 +0330

    [ADD] ReverseArray function

---
+++ hello-typescript/src/main.ts
@@ -0,0 +1,8 @@
+import reverseArray from "./reverse-array";
+
+
+// Task 1
+console.info("Task 1")
+const ar = ["A", 2, true, , null]
+console.log(ar, "=>" ,reverseArray(ar))
+console.info("###################")
